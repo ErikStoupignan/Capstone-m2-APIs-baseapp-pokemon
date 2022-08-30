7f817a0d8edaed5a1e235985a08742b0a4b4a806
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-08-30 16:56:28 -0500

    Merge between version, fix style error in body+API

--- src/modules/PokemonAPI.js
+++ src/modules/PokemonAPI.js
@@ -1,9 +1,15 @@
 class Pokemon {
-
   async getData(id) {
     const requesting = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const data = await requesting.json();
-    return data;
-    }
+    this.data = await requesting.json();
+    return this.data;
+  }
+
+  async getList() {
+    const listrequest = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=60&offset=0');
+    this.dataList = await listrequest.json();
+    return this.dataList;
+  }
 }
+
 export default Pokemon;
