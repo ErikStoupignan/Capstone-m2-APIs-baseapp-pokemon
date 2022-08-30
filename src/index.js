7cf2437503562e0d58fbc45e6776e31d1da0689f
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-08-30 16:57:16 -0500

    Merge index versions, fix style.

--- src/index.js
+++ src/index.js
@@ -3,46 +3,40 @@ import './css/pokeball.css';
 import './css/popUp-menu.css';
 
 document.body.addEventListener('click', (e) => {
-  const {id} = e.target;
-  console.log('Id del elemento seleccionado:',id);
-  const regexBtnCom =  /(?<=button)\d+$/;
-  const regexLike =  /(?<=likeIcon)\d+$/;
+  const { id } = e.target;
+  console.log('Id del elemento seleccionado:', id);
+  const regexBtnCom = /(?<=button)\d+$/;
+  const regexLike = /(?<=likeIcon)\d+$/;
   const closeModalId = 'xmodal';
   const submitbtn = 'btnSubmit';
 
-  // Say the id of the button 
-  if(regexBtnCom.test(id)) { 
+  // Say the id of the button
+  if (regexBtnCom.test(id)) {
     console.log(id);
     document.getElementById('popUp-menu').classList.toggle('display-none');
-  } 
+  }
 
   // Point to the x on the popUp menu
-  if(id === closeModalId) {
+  if (id === closeModalId) {
     document.getElementById('popUp-menu').classList.toggle('display-none');
   }
 
   // Recovery the data from the inputs on the popUp menu
-  if(id === submitbtn) {
+  if (id === submitbtn) {
     const inputName = document.getElementById('input-text').value;
     const inputText = document.getElementById('input-comment').value;
-    if(inputName === '' && inputText === '') return;
+    if (inputName === '' && inputText === '') return;
 
     document.getElementById('input-text').value = '';
     document.getElementById('input-comment').value = '';
 
-    console.log('name:', inputName, 'comment:',inputText);
+    console.log('name:', inputName, 'comment:', inputText);
   }
 
   // Read the like Icon
-  if(regexLike.test(id)) {
+  if (regexLike.test(id)) {
     const like = document.getElementById(`${id}`);
-    like.style.color= 'red';
-    console.log('Like en la tarjeta número:', id , like);
+    like.style.color = 'red';
+    console.log('Like en la tarjeta número:', id, like);
   }
-
-  return null;
 });
-
-
-
-
