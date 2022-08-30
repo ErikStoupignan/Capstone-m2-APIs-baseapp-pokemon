42989fbd964ac1c929bbdeb65059787a251fa702
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-08-30 12:46:31 -0500

    Add eventlistener to the buttons on the html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './style.css';
 import './css/pokeball.css';
 import './css/popUp-menu.css';
+
+document.body.addEventListener('click', (e) => {
+  const {id} = e.target;
+  const regex =  /(?<=button)\d+$/;
+  const closeModalId = 'xmodal'
+
+  if(regex.test(id)) { 
+    console.log(id);
+    document.getElementById('popUp-comments').classList.toggle('popUp-window');
+  } 
+
+  if(id === closeModalId) {
+    document.getElementById('popUp-comments').classList.toggle('popUp-window');
+  }
+
+  
+  return null;
+});
+
+// document.getElementById('xmodal').addEventListener('click', () =>{ 
+//   document.getElementById('popUp-comments').classList.toggle('popUp-window');
+// });
+
+
+
+
