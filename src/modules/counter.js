cea1555cc6da0f19c186cd7fde9f85ccfd0b6763
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-09-01 17:56:52 -0500

    Fix linters details

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,7 +1,3 @@
-import pokemonCounter from "./PokemonCounter.js";
-import { counter } from './DOMelements.js';
-import myArray from './Array.js';
-
 // Counter comments
 const counterComments = () => {
   const result = document.querySelectorAll('.commit');
